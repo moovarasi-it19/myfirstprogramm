0bd24d3979961d86f11f616ba91624e6987fd82c
moovarasi-it19 <54206322+moovarasi-it19@users.noreply.github.com>
2019-11-14 11:54:42 +0530

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,11 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+d=int(input("enter the number"))
+e=int(input("enter the number"))
+total=a+b+c+d+e
+print(total)
+if((a>30)and(b>30)and(c>30)and(d>30)and(e>30)):
+ print("pass")
+else:
+ print("fail")
